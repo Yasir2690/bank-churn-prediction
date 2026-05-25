589d31d7f32013ecdf297831dff85e365f208dd1
Yasir Asfaque <yasirasfaque91@gmail.com>
2026-05-26 00:43:39 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Minimal Test")
+
+st.title("Hello World!")
+st.write("If you can see this, Python 3.9 is working!")
+
+st.success("✅ Deployment successful!")
